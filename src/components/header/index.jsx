3066c2b80f4e2726d9a2a6068b226246211acd01
Jose Caicedo <jose.caicedo@outlook.com>
2023-07-10 14:54:32 -0500

    deleted logo header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,8 @@
-import LogoSVG from "./logo"
-
 export const Header = () => {
   return (
     <header className="text-gray-600 body-font bg-blue-500/90">
       <div className="container mx-auto flex flex-wrap py-3 px-4 flex-col md:flex-row items-center">
         <a href='/' className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 cursor-pointer">
-          <LogoSVG width={150} color={"#fff"} />
           <span className="ml-3 text-xl text-white">Antara</span>
         </a>
         <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center text-white gap-3">
